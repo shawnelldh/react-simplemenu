5862a0ab37bd9d200c8a912e2b03587143878557
shawnelldh <shawnelldh@gmail.com>
2021-04-09 12:20:40 -0400

    5 binaries uploaded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import App from "./App";
+// Components.
+import FoodList from './components/FoodList'
+import FoodAdd from './components/FoodAdd'
+import FoodEdit from './components/FoodEdit';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import './css/reset.css'
+import './css/base.css'
+import './css/header.css'
+import './css/style.css'
+
+// Layout.
+import Header from './layout/Header'
+import Footer from './layout/Footer'
+
+const pageNotFound = ({ location }) => (
+  <div className="container" style={{padding: '50px 0'}}>
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+);
+
+render((
+  <Router>
+    <div className="page-wrapper">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={FoodList}/>
+        <Route path="/add" exact component={FoodAdd}/>
+        <Route path="/:id/edit" component={FoodEdit}/>
+        <Route component={pageNotFound} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+), document.getElementById('root'));
